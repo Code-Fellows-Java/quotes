a8859e8b10d235c9de51fd337cf52195cc9dca9e
Ian <ian.forrester46@gmail.com>
2022-11-02 17:48:25 -0500

    Lab 08 2

--- app/src/test/java/quotes/AppTest.java
+++ app/src/test/java/quotes/AppTest.java
@@ -14,8 +14,9 @@ public class AppTest {
         Quote quote = new Quote("Marilyn Monroe", "I am good, but not an angel. I do sin, but I am not the devil. I am just a small girl in a big world trying to find someone to love.");
         Quote quote1 = new Quote("Charles Dickens", "Ask no questions, and you'll be told no lies.");
         Quote quote2 = new Quote("George Orwell", "If liberty means anything at all it means the right to tell people what they do not want to hear.");
+        Quote quote3 = new Quote("Graham Greene", "We'd forgive most things if we knew the facts.");
 
-        Quote[] quotes = {quote, quote1, quote2 };
+        Quote[] quotes = {quote, quote1, quote2, quote3 };
         Quote randomQuote = App.getRandoQuote(quotes);
 
         assertNotNull(randomQuote);
